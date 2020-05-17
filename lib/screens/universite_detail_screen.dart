1f373a75b5ea98917b4234e701f5cb4b79ed4e2d
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-05-17 15:50:02 +0300

    Paketler güncellendi.

--- lib/screens/universite_detail_screen.dart
+++ lib/screens/universite_detail_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:json_table/json_table.dart';
-import 'package:json_table/json_table_column.dart';
 import 'package:http/http.dart' as http;
 import 'package:uniapp/settings/constants.dart';
 
